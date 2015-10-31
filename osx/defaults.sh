906d47fd67552a4bdc0d69222313c86922cbe7cf
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2015-10-31 18:26:46 +0900

    Add osx defaults
    Add dashboard settings defaults

---
+++ osx/defaults.sh
@@ -0,0 +1,12 @@
+###############################################################################
+# Dashboard                                                                   #
+###############################################################################
+
+# Speed up Mission Control animations
+defaults write com.apple.dock expose-animation-duration -float 0.0
+
+# Disable Dashboard
+defaults write com.apple.dashboard mcx-disabled -bool true
+
+# Don’t show Dashboard as a Space
+defaults write com.apple.dock dashboard-in-overlay -bool true
