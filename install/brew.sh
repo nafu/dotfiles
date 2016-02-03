417b519453598624d8121102339614bdc8a06827
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2016-02-03 14:35:09 +0900

    Fix indentation

--- install/brew.sh
+++ install/brew.sh
@@ -6,21 +6,21 @@ brew upgrade
 # Install packages
 
 apps=(
-    git
-    hub
-    # Add --with-brewed-openssl for El Capitan. https://github.com/Homebrew/homebrew/issues/40519#issuecomment-110468872
-    vim --with-brewed-openssl
-    wget
-    awscli
-    ack
-    the_silver_searcher
-    # ruby
-    rbenv
-    ruby-build
-    # database
-    postgresql
-    redis
-    heroku-toolbelt
+  git
+  hub
+  # Add --with-brewed-openssl for El Capitan. https://github.com/Homebrew/homebrew/issues/40519#issuecomment-110468872
+  vim --with-brewed-openssl
+  wget
+  awscli
+  ack
+  the_silver_searcher
+  # ruby
+  rbenv
+  ruby-build
+  # database
+  postgresql
+  redis
+  heroku-toolbelt
 )
 
 brew install "${apps[@]}"
