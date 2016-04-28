5c76aaacd5cd3d330ad41c401926362d9f2fb85e
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2016-04-28 22:03:34 +0900

    Add list of redshift_requirements.txt to pip.sh

--- install/pip.sh
+++ install/pip.sh
@@ -3,6 +3,12 @@
 apps=(
   # In use evervim
   markdown
+  # redshift_requirements.txt
+  # https://github.com/3mei/rjmetrics_blog_posts/blob/master/jupyter_redshift_analytics_stack/assets/redshift_requirements.txt
+  psycopg2
+  sqlalchemy
+  ipython-sql
+  simplejson
 )
 
 pip install "${apps[@]}"
