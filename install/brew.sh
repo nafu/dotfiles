52914418f4842aa247158b0fe3d0cdacff004151
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2023-05-09 17:31:43 +0900

    Update vim install options

--- install/brew.sh
+++ install/brew.sh
@@ -21,8 +21,7 @@ apps=(
   peco
   # Required to use neocomplete.vim
   lua
-  # Add --with-brewed-openssl for El Capitan. https://github.com/Homebrew/homebrew/issues/40519#issuecomment-110468872
-  vim --with-brewed-openssl --with-lua
+  vim
   wget
   awscli
   ack
